c8e21915515c6a8d87c588380b69e5982d3d8278
Vladimir Kolesnikov <9858713@gmail.com>
2026-01-08 13:43:50 +0200

    refactor: extract database setup into separate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,10 @@
-// import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbModule } from './db.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
